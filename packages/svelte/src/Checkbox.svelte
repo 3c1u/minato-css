fcd7a63e4118836bc2dc4c5a9e7aa5b6cded30bf
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2021-02-13 20:00:20 +0900

    @minato-css/svelte: add components

---
+++ packages/svelte/src/Checkbox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { createEventDispatcher } from 'svelte'
+const dispatch = createEventDispatcher()
+
+export let checked: boolean = false
+export let disabled: boolean = false
+
+const handleChange = () => {
+  dispatch('change', checked)
+}
+</script>
+
+<label class="mcss-checkbox-container">
+  <input class="mcss-checkbox" type="checkbox" bind:checked {disabled} on:change={handleChange} />
+  <span class="mcss-checkbox-label"><slot /></span>
+</label>
--- packages/svelte/src/index.js
+++ packages/svelte/src/index.js
@@ -2,7 +2,8 @@ import '@minato-css/minato-css'
 import Button from './Button.svelte'
 import ButtonGroup from './ButtonGroup.svelte'
 import Card from './Card.svelte'
+import Checkbox from './Checkbox.svelte'
 import Spacing from './Spacing.svelte'
 import TextInput from './TextInput.svelte'
 
-export { Button, ButtonGroup, Card, TextInput, Spacing }
+export { Button, ButtonGroup, Card, Checkbox, TextInput, Spacing }
