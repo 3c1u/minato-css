c77697c55c5ca480cc7c05291acd75c8e2cb4968
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2021-02-14 18:03:14 +0900

    @mitano-css/react: fix build

--- packages/react/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("@minato-css/minato-css");
-var Button_1 = require("./Button");
-exports["default"] = {
-    Button: Button_1["default"]
-};
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,28 @@
 import '@minato-css/minato-css'
 import Button from './Button'
+import ButtonGroup from './ButtonGroup'
+import Card from './Card'
+import Checkbox from './Checkbox'
+import CheckboxGroup from './CheckboxGroup'
+import Dropdown from './Dropdown'
+import DropdownItem from './DropdownItem'
+import Radio from './Radio'
+import RadioGroup from './RadioGroup'
+import Spacing from './Spacing'
+import Switch from './Switch'
+import TextInput from './TextInput'
 
-export default {
+export {
   Button,
+  ButtonGroup,
+  Card,
+  Checkbox,
+  CheckboxGroup,
+  Dropdown,
+  DropdownItem,
+  Radio,
+  RadioGroup,
+  Spacing,
+  Switch,
+  TextInput,
 }
